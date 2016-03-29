56f9b0728983c79decd5a61715247c96a8903e91
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-29 15:43:26 -0500

    Commit 3/29/16 Modified high_low.php 	-Changed rand -> mt_rand

--- high_low.php
+++ high_low.php
@@ -2,8 +2,26 @@
 $min = 1;
 $max = 100;
 
+// do {
+// 	$random = rand($min, $max);
+
+// 	do {
+// 		fwrite(STDOUT, "Guess? ");
+// 		$guess = fgets(STDIN) . PHP_EOL;
+// 		if ($guess == $random) {
+// 			fwrite(STDOUT, "You Win! ") . PHP_EOL;
+// 			fwrite(STDOUT, "Hit enter to play again") . PHP_EOL;
+// 			fgets(STDIN);
+// 		} elseif ($guess > $random) {
+// 			fwrite(STDOUT, "Lower! " . PHP_EOL);
+// 		} else if ($guess < $random) {
+// 			fwrite(STDOUT, "Higher! " . PHP_EOL);
+// 		} 
+// 	} while ($guess != $random);
+// } while (1 == 1);
+
 do {
-	$random = rand($min, $max);
+	$random = mt_rand($min, $max);
 
 	do {
 		fwrite(STDOUT, "Guess? ");
