d10637df521424f4653a55f0fee2249cd791fbc7
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 11:22:30 -0500

    Commit 3/30 Added FizzBuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,23 +1,22 @@
 <?php
-  $fizz = 3;
-  $buzz = 5;
-  $fizzbuzz = 15;
 
-  echo "\n";
-  for ($i = 1; $i <= 100; $i+=1) {
-    if ($i == $fizzbuzz) {
-      echo "$i Fizzbuzz\n";
-      $fizzbuzz += 15;
-      $fizz += 3;
-      $buzz += 5;
-    } else if ($i == $fizz) {
-      echo"$i Fizz\n";
-      $fizz += 3;
-    } else if ($i == $buzz) {
-      echo"$i Buzz\n";
-      $buzz += 5;
-    } else {
-      echo "$i\n";
-    }
-  }
-?>
+$fizz = 3;
+$buzz = 5;
+$fizzbuzz = 15;
+
+for($i = 0; $i <= 100; $i++) {
+	if ($i == $fizzbuzz) {
+		echo "FizzBuzz" . PHP_EOL;
+		$fizzbuzz += 15;
+		$fizz += 3;
+		$buzz += 5;
+	} else if ($i == $fizz) {
+		echo "Fizz" . PHP_EOL;
+		$fizz += 3;
+	} else if ($i ==$buzz) {
+		echo "Buzz" . PHP_EOL;
+		$buzz += 3;
+	} else {
+		echo $i . PHP_EOL;
+	}
+}
