5cea83df4104e0339264a9b7b527eb6f1c0d6678
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 09:17:22 -0500

    Commit 3/30 Modified for.php 	-Added user input for incrementation

--- for.php
+++ for.php
@@ -4,10 +4,11 @@ fwrite(STDOUT, "Enter a number" . PHP_EOL);
 $numberOne = fgets(STDIN);
 fwrite(STDOUT, "Enter another number" . PHP_EOL);
 $numberTwo = fgets(STDIN);
-
+fwrite(STDOUT, "Enter a number to increment by" . PHP_EOL);
+$increment = fgets(STDIN);
 
 echo "\nNumber One: {$numberOne}Number Two: {$numberTwo}";
 
-for ($i = (int)$numberOne; $i < (int)$numberTwo; $i++) {
+for ($i = (int)$numberOne; $i < (int)$numberTwo; $i+=$increment) {
 	echo $i . PHP_EOL;
 };
