af5cca9c406374b5e13a5a67a2460eb971049c63
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 10:42:46 -0500

    Commit 3/30 Modified for.php 	-Gave up on trying to implement error message

--- for.php
+++ for.php
@@ -7,12 +7,17 @@ $numberTwo = fgets(STDIN);
 fwrite(STDOUT, "Enter a number to increment by" . PHP_EOL);
 $increment = fgets(STDIN);
 
-if (!is_null($increment) && $increment != 0) {
-	for ($i = (int)$numberOne; $i <= (int)$numberTwo; $i+=(int)$increment) {
-		echo $i . PHP_EOL;
-	}
-} else {
-	for ($i = (int)$numberOne; $i <= (int)$numberTwo; $i++) {
-		echo $i . PHP_EOL;
+echo PHP_EOL;
+
+if (is_numeric((int)$numberOne) || is_numeric((int)$numberTwo) || is_numeric((int)$increment)) {
+
+	if (!is_null($increment) && $increment != 0) {
+		for ($i = (int)$numberOne; $i <= (int)$numberTwo; $i+=(int)$increment) {
+			echo $i . PHP_EOL;
+		}
+	} else {
+		for ($i = (int)$numberOne; $i <= (int)$numberTwo; $i++) {
+			echo $i . PHP_EOL;
+		}
 	}
 }
