254e53ecf1c312d2437ff09f26f012f3f04eaa3e
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 09:15:23 -0500

    Commit 3/30 Added for.php

--- for.php
+++ for.php
@@ -1,43 +1,13 @@
 <?php
-  
-   (int)$userNumOne = 0;
-   (int)$userNumTwo = 0;
-   (int)$userInterval = 0;
 
-  fwrite(STDOUT, "\nPlease enter a number.\n");
-  do {
-    $userNumOne = fgets(STDIN);
-    var_dump((int)$userNumOne);
-    if (is_int((int)$userNumOne) == false) {
-      fwrite(STDOUT, "You did not enter a valid number.\n");
-      fwrite(STDOUT, "Enter a valid number.\n");
-      $userNumOne = fgets(STDIN);
-    }
-  } while (is_int((int)$userNumOne) == false);
+fwrite(STDOUT, "Enter a number" . PHP_EOL);
+$numberOne = fgets(STDIN);
+fwrite(STDOUT, "Enter another number" . PHP_EOL);
+$numberTwo = fgets(STDIN);
 
-  fwrite(STDOUT, "\nPlease enter another number.\n");
-  do {
-    $userNumTwo = fgets(STDIN);
-    var_dump((int)$userNumTwo);
-    if (is_int((int)$userNumTwo) == false) {
-      fwrite(STDOUT, "You did not enter a valid number.\n");
-      fwrite(STDOUT, "Enter a valid number.\n");
-      $userNumTwo = fgets(STDIN);
-    }
-  } while (is_int((int)$userNumTwo) == false);
 
-  fwrite(STDOUT, "\nPlease enter an interval.\n");
-  do {
-    $userInterval = fgets(STDIN);
-    var_dump((int)$userInterval);
-    if (is_int((int)$userInterval) == false) {
-      fwrite(STDOUT, "You did not enter a valid number.\n");
-      fwrite(STDOUT, "Enter a valid number.\n");
-      $userInterval = fgets(STDIN);
-    }
-  } while (is_int((int)$userInterval) == false);
+echo "\nNumber One: {$numberOne}Number Two: {$numberTwo}";
 
-  for ((int)$i = (int)$userNumOne; $i <= (int)$userNumTwo; (int)$i+=$userInterval) {
-    echo "\$i has the value of {$i}\n";
-  }
-?>
+for ($i = (int)$numberOne; $i < (int)$numberTwo; $i++) {
+	echo $i . PHP_EOL;
+};
