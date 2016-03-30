1dc68054b846c83fa7cf388db251bc4d3ae7dddb
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 11:57:03 -0500

    Commit 3/30/16 Modified foreach.php 	-Added is_scalar function

--- foreach.php
+++ foreach.php
@@ -9,26 +9,33 @@
 //     }
 // }
 
-$array = array(1,2,3);
+// $array = array(1,2,3);
 
-$things = array('Sgt. Pepper', "11", null, $array, 3.14, "12 + 7", false, (string) 11);
+// $things = array('Sgt. Pepper', "11", null, $array, 3.14, "12 + 7", false, (string) 11);
+
+// foreach($things as $thing) {
+// 	if (is_string($thing)) {
+// 		echo $thing . PHP_EOL;
+// 	} elseif (is_null($thing)) {
+// 		echo PHP_EOL;
+// 	} elseif (is_array($thing)) {
+// 		echo "Array (";
+// 		foreach($array as $index) {
+// 			echo $index;
+// 		}
+// 		echo ")" . PHP_EOL;
+// 	} elseif (is_numeric($thing)) {
+// 		echo $thing . PHP_EOL;
+// 	} elseif (is_bool($thing)) {
+// 		echo PHP_EOL;
+// 	} elseif (is_int($thing)) {
+// 		echo $i . PHP_EOL;
+// 	}
+// }
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach($things as $thing) {
-	if (is_string($thing)) {
+	if (is_scalar($thing)) {
 		echo $thing . PHP_EOL;
-	} elseif (is_null($thing)) {
-		echo PHP_EOL;
-	} elseif (is_array($thing)) {
-		echo "Array (";
-		foreach($array as $index) {
-			echo $index;
-		}
-		echo ")" . PHP_EOL;
-	} elseif (is_numeric($thing)) {
-		echo $thing . PHP_EOL;
-	} elseif (is_bool($thing)) {
-		echo PHP_EOL;
-	} elseif (is_int($thing)) {
-		echo $i . PHP_EOL;
 	}
 }
