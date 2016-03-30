4dbbac9d0879b250537ff8b4a43fd7d17d28928c
Gabriel Campbell <Gabe_Campbell1@Gabriels-MacBook-Air.local>
2016-03-30 11:54:55 -0500

    Commit 3/30/16 Modified foreach.php 	-Finished functions

--- foreach.php
+++ foreach.php
@@ -9,5 +9,26 @@
 //     }
 // }
 
-$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+$array = array(1,2,3);
 
+$things = array('Sgt. Pepper', "11", null, $array, 3.14, "12 + 7", false, (string) 11);
+
+foreach($things as $thing) {
+	if (is_string($thing)) {
+		echo $thing . PHP_EOL;
+	} elseif (is_null($thing)) {
+		echo PHP_EOL;
+	} elseif (is_array($thing)) {
+		echo "Array (";
+		foreach($array as $index) {
+			echo $index;
+		}
+		echo ")" . PHP_EOL;
+	} elseif (is_numeric($thing)) {
+		echo $thing . PHP_EOL;
+	} elseif (is_bool($thing)) {
+		echo PHP_EOL;
+	} elseif (is_int($thing)) {
+		echo $i . PHP_EOL;
+	}
+}
